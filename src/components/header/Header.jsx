52d46a8a2e8a318e0d68aa749e0ae4f89119d6cb
Dmitriy <aegis.ko20@mail.ru>
2024-08-06 10:34:07 +0300

    header fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -67,16 +67,51 @@ const Header = () => {
 
 	return (
 		<header
-			className={`${styles.header} text-color_three bg-color_two w-full h-12 flex justify-between items-center px-9 TABLET1024:px-5 MOBILE768:flex-wrap MOBILE768:h-24 MOBILE768:pt-3 MOBILE768:px-4`}
+			className={`${styles.header} text-color_three bg-color_two w-full h-12 flex justify-between items-center px-9 TABLET1024:px-5  MOBILE869:h-24 MOBILE869:flex-col MOBILE869:pt-3 MOBILE869:px-4`}
 		>
+			<div className=' hidden MOBILE869:flex items-center justify-between w-full'>
+				<h1
+					className={`${styles.logo} text-base18 font-black uppercase hover:cursor-pointer hover:text-color_eight`}
+					onClick={() => nav('/')}
+				>
+					Cryptonit
+				</h1>
+				<div
+					className={`${styles.block__input} relative -ml-96 mr-80 flex items-center gap-1 TABLET1024:-ml-3 TABLET1024:mr-0 PK1366:-ml-20 PK1625:-ml-4 PK1300:-mr-0 `}
+				>
+					<img
+						src='/images/icons/search.svg'
+						alt='search'
+						className={`${styles.icon__search} absolute left-1 w-5 h-5`}
+					/>
+					<input
+						type='text'
+						className={`${styles.input} bg-color_ten rounded-3xl w-80 h-7 text-color_three pl-7 pt-1 outline-none MOBILE869:w-52 MOBILE430:w-40`}
+					/>
+					<button
+						className={`${styles.mbSelect} bg-color_ten rounded-3xl h-7 w-12 flex items-center gap-1 justify-center`}
+					>
+						<img
+							src='/images/icons/search_S.svg'
+							alt='searchS'
+							className={`${styles.icon__searchS} w-3 h-3`}
+						/>
+						<img
+							src='/images/icons/arrow-down.svg'
+							alt='arrow'
+							className={`${styles.icon__arrow} w-4 h-4`}
+						/>
+					</button>
+				</div>
+			</div>
 			<h1
-				className={`${styles.logo} text-base18 font-black uppercase hover:cursor-pointer hover:text-color_eight`}
+				className={`${styles.logo} text-base18 font-black uppercase hover:cursor-pointer hover:text-color_eight MOBILE869:hidden`}
 				onClick={() => nav('/')}
 			>
 				Cryptonit
 			</h1>
 			<div
-				className={`${styles.block__input} relative -ml-96 mr-80 flex items-center gap-1 TABLET1024:-ml-3 TABLET1024:mr-0 PK1366:-ml-20 PK1625:-ml-4 PK1300:-mr-0`}
+				className={`${styles.block__input} relative -ml-96 mr-80 flex items-center gap-1 TABLET1024:-ml-3 TABLET1024:mr-0 PK1366:-ml-20 PK1625:-ml-4 PK1300:-mr-0 MOBILE869:hidden`}
 			>
 				<img
 					src='/images/icons/search.svg'
@@ -85,7 +120,7 @@ const Header = () => {
 				/>
 				<input
 					type='text'
-					className={`${styles.input} bg-color_ten rounded-3xl w-80 h-7 text-color_three pl-7 pt-1 outline-none MOBILE768:w-52 MOBILE768:w-40`}
+					className={`${styles.input} bg-color_ten rounded-3xl w-80 h-7 text-color_three pl-7 pt-1 outline-none MOBILE869:w-52 `}
 				/>
 				<button
 					className={`${styles.mbSelect} bg-color_ten rounded-3xl h-7 w-12 flex items-center gap-1 justify-center`}
@@ -102,12 +137,13 @@ const Header = () => {
 					/>
 				</button>
 			</div>
+
 			<div
-				className={`${styles.block__right} flex gap-12 items-center TABLET1024:gap-7`}
+				className={`${styles.block__right} flex gap-12 items-center TABLET1024:gap-7 MOBILE869:justify-between MOBILE869:w-full `}
 			>
 				<Link
 					to='/about'
-					className={`${styles.link__about} text-color_three MOBILE375:mr-12 hover:text-color_eight MOBILE430:mr-24 MOBILE768:mr-72 MOBILE624:mr-32`}
+					className={`${styles.link__about} text-color_three  hover:text-color_eight `}
 				>
 					About us
 				</Link>
